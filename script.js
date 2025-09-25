a403caeb47ece1277845cdc0decd36911cf7870b
Marc-AntoineMarie <mmarie.galaxie@gmail.com>
2025-09-25 23:12:36 +0200

    last commit for the night

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 document.getElementByid("random-btn").addEventListener("click", () => {
     console.log("Randomize Button Clicked");
 });
+
+for (const cat in categories) {
+    const category = categories[cat];
+    category.index  = Math.floor(Math.random() * category.images.lenght);
+    updateImage(cat);
+}
+
+document.getElementByid("random-btn").addEventListener("click", randomize);
